dd7b931884944a3837a8a81c9bc8758fddde4c77
teeteetee <proto@protos-MacBook-Pro.local>
2015-08-19 05:12:25 -0700

    --

--- app.js
+++ app.js
@@ -22,8 +22,8 @@ app.set('view engine', 'jade');
 
 app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
-app.use(require('connect').bodyParser());
-app.use(bodyParser.urlencoded());
+//app.use(require('connect').bodyParser());
+app.use(bodyParser.urlencoded(extended: true));
 app.use(cookieParser());
 app.use(express.compress());
 app.use(express.static(path.join(__dirname, 'public'), { maxAge: 2540000000 }));
