0be40f7706fb08ae2c9f9d5ceb82bc1989193dea
teeteetee <proto@protos-MacBook-Pro.local>
2015-08-21 08:05:38 -0700

    --

--- public/js/crop.js
+++ public/js/crop.js
@@ -73,8 +73,7 @@ jQuery(function($){
          method: "POST",
          url: "/userp/crop",
          data: {'x1': x1, 'x2': x2, 'y1': y1, 'y2': y2, 'img': img, 'crop': crop}
-       })
-         .done(
+       }).done(
          	//function( jqXHR ) {
            //var response = JSON.parse(jqXHR.responseText);
            //alert(response.rdurl);
