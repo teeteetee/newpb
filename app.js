1a20a0c33358c71d1f7d5abe5275bc24e70ca6e9
teeteetee <proto@protos-MacBook-Pro.local>
2015-08-31 05:14:45 -0700

    --

--- app.js
+++ app.js
@@ -361,7 +361,7 @@ app.post('/getavatar/:uid',function (req,res){
        }
        else {
          if(doc){
-            if(parseInt(doc.userpic))
+            if(parseInt(doc.userpic)!=0)
             {ms.trouble = 0;
              ms.mtext = doc.userpic;
              res.send(ms);}
