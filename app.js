b98af68df4df815ab5f46021979a2677ad1669c6
teeteetee <proto@protos-MacBook-Pro.local>
2015-09-03 05:01:21 -0700

    --

--- app.js
+++ app.js
@@ -274,7 +274,7 @@ app.get('/discussion/:id',function (req,res){
 
 app.post('/settings',function (req,res){
   var ms={};
-  var ms.trouble=1;
+  ms.trouble=1;
   var vpub = parseInt(req.body.pub);// public
   var vmrq = parseInt(req.body.mrq);// messaging request
   if(req.session.mail){
