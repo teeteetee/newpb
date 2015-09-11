f393d9768f2173969745fbcc40f764a2b349cd56
teeteetee <proto@protos-MacBook-Pro.local>
2015-09-11 05:10:47 -0700

    --

--- app.js
+++ app.js
@@ -384,7 +384,7 @@ app.post('/markread/:uid/:bid',function (req,res){
        var temp_arr;
        temp_arr = doc.bookstore;
        for(var i=0;i<temp_arr.length;i++){
-         console.log(temp_arr[i]._id);
+         console.log(typeof temp_arr[i]._id+', and '+typeof req.params.bid);
          if(temp_arr[i]._id === req.params.bid){
            console.log('modifying');
            temp_arr[i].newbook = 0;
