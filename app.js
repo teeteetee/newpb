8327c86347dbdb153934bba724f0e8e6f443aeaf
teeteetee <proto@protos-MacBook-Pro.local>
2015-09-12 14:03:49 -0700

    --

--- app.js
+++ app.js
@@ -793,6 +793,7 @@ app.post('/disccheck/:id/:uid',function (req,res){
         }
         if(vlsttmstmp&&vlsttmstmp!=null){
             console.log('setting lsttmstmp: '+vlsttmstmp);
+            console.log('discid: '+vdiscid);
             eval("db.collection('users').update({uid:rcvr},{$set:{tmpstmpstore."+vdiscid+":vlsttmstmp}});");
               }
         ms.trouble=0;
