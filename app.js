62bbf5bc36c7027f3f34a251721e0263832b9fce
teeteetee <proto@protos-MacBook-Pro.local>
2015-09-13 12:11:56 -0700

    --

--- app.js
+++ app.js
@@ -785,18 +785,18 @@ app.post('/gtm/:discid',function(req,res){
             for (var i = tmp_l; i>0; i--) {
                console.log('i: '+i);
                console.log('long poll in 7');
+               if(doc.msgstore[i].tmstmp <= vtmstmp){
+                       console.log('long poll in 10');
+                       break;
+                     }
               if(doc.msgstore[i].rcvr === parseInt(req.session.uid) && doc.msgstore[i].tmstmp > vtmstmp) {
                     console.log('long poll in 8');
                     ms.msgstore.push(doc.msgstore[i]);
-                    if(i=doc.msgstore.length-1) {
+                    if(i=tmp_l) {
                       console.log('long poll in 9');
                      vlsttmstmp=doc.msgstore[i].tmstmp;
                    }
                   }
-              if(doc.msgstore[i].tmstmp <= vtmstmp){
-                console.log('long poll in 10');
-                break;
-              }
             }
          if(ms.msgstore.length)   
         {console.log('long poll in 11');
