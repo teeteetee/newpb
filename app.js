cb4a92bbc3757d98fe8670af18c28078c6a41c18
teeteetee <proto@protos-MacBook-Pro.local>
2015-09-19 07:02:07 -0700

    --

--- app.js
+++ app.js
@@ -1013,7 +1013,7 @@ app.get('/id:id', function (req,res){
                           res.render('anotheruser',{'user':doc.uid,'avatar':avatar,'doc':JSON.stringify(doc)});
                       }
                        else {
-                        var emptyavatar = "div id=emptyavatar class='img-circle pull-left' style='width:193px;height:193px;margin:30;margin-left:50;margin-right:50;border:7px dotted #eee;bakcground-color:white;border-radius:50%;'";
+                        var emptyavatar = "div id=emptyavatar class='img-circle pull-left'";
                         res.render('anotheruser',{'user':doc.uid,'avatar':emptyavatar,'doc':JSON.stringify(doc)});
                        }
                   }
