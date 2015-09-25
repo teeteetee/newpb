05d6a1bd0e6aba9986cd6fe6170dd50fdb35d25a
teeteetee <proto@protos-MacBook-Pro.local>
2015-09-25 11:01:07 -0700

    --

--- app.js
+++ app.js
@@ -1059,7 +1059,7 @@ app.post('/unfollow/:id',function (req,res){
     var tmp_unset={}
     tmp_unset[tmp_id]=0;
    follow.update({user:req.session._id},{$unset:tmp_unset});
-   for(var i =0; i< req.session.userstore.length;i++;){
+   for(var i =0; i< req.session.userstore.length;i++){
     console.log(req.session.userstore[i]+','+typeof req.session.userstore[i]);
     console.log('\n'+tmp_id);
   if ( req.session.userstore[i] === tmp_id) { req.session.userstore.splice(i, 1);break}
