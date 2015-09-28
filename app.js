8def68fde677d204371b7f9925f583df9fc9c2e7
teeteetee <proto@protos-MacBook-Pro.local>
2015-09-28 11:39:39 -0700

    --

--- app.js
+++ app.js
@@ -819,7 +819,7 @@ app.post('/gtm/:discid',function(req,res){
         {
           var sht_tmp ={};
            sht_tmp['$set'] = {};
-           sht_tmp['$set'['tmstmpstore'[g_vdiscid]]] = vlsttmstmp;
+           sht_tmp['$set']['tmstmpstore'][g_vdiscid] = vlsttmstmp;
            console.log('sht_tmp: '+JSON.stringify(sht_tmp));
            users.update({_id:req.session._id},sht_tmp);
                 
