9d3019fa73231c025fb2c9f60afc43c4fb17a849
teeteetee <proto@protos-MacBook-Pro.local>
2015-10-08 12:32:25 -0700

    --

--- app.js
+++ app.js
@@ -711,9 +711,9 @@ app.get('/seefollow',function (req,res){
 
 app.get('/clearbooks',function (req,res){
   if(req.session._id)
-  {users.update({_id:req.session._id},{$unset:{bookstore:[]},$set:{totalbooks:0,readbooks:0,newbooks:0}});
+  {users.update({_id:req.session._id},{$set:{bookstore:[]},$set:{totalbooks:0,readbooks:0,newbooks:0}});
     console.log('empty items');
-    items.update({user:req.session._id.toString()},{$unset:{bookstore:[]}});
+    items.update({user:req.session._id.toString()},{$set:{bookstore:[]}});
     res.redirect('/');}
     else {
       res.redirect('/');
