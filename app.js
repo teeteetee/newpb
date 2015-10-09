c618998e51e423b2ee2002382bbb986d6709b12f
teeteetee <proto@protos-MacBook-Pro.local>
2015-10-09 14:57:13 -0700

    --

--- app.js
+++ app.js
@@ -1305,7 +1305,7 @@ app.get('/people/:kind/:item',function (req,res){
   console.log('\n params: '+req.params.item);
     switch(req.params.kind){
        case('b'):
-       db.collection('items').find({bookstore:req.params.item}, function(err,done){
+       db.collection('items').find({bookstore:req.params.item.toString()}, function(err,done){
          res.send(done);
        });
        break;
