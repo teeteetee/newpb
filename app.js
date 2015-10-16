8d80dbbbfc019d6263a0790d2f36f82abafdcd7f
teeteetee <proto@protos-MacBook-Pro.local>
2015-10-16 09:54:21 -0700

    --

--- app.js
+++ app.js
@@ -1797,7 +1797,7 @@ app.post('/userp/crop',function (req,res){
        console.log('CROPPING');
        image.crop(_cropOpt.left, _cropOpt.top, _cropOpt.right, _cropOpt.bottom, function(err, crpdImg) {
          if (err) throw err;
-         console.log('loading crpdImg');
+         console.log('loading crpdImg with extension: '+path.extname(imgname));
          lwip.open(crpdImg,path.extname(imgname) , function(err, pre_final_image) {
            if (err) throw err;
            console.log('cropped image width: '+pre_final_crpdImg.width);
