f5ba4971693ad99d415fa3ba1232cfe83fdc9fac
teeteetee <proto@protos-MacBook-Pro.local>
2015-11-03 14:27:23 -0800

    --

--- app.js
+++ app.js
@@ -1665,7 +1665,7 @@ app.get('/u/:nick', function (req,res){
                     done=[];
                   }
                   doc.bookstore = done.bookstore;
-                  console.log(doc.bookstore);
+                  console.log(JSON.stringify(doc.bookstore));
                   doc.moviestore = done.moviestore;
                   doc.articlestore = done.articlestore;
                   console.log('item done: '+done);
