15307fa56ed7001a74401096552a752772ea89d9
teeteetee <proto@protos-MacBook-Pro.local>
2015-11-15 15:55:54 -0800

    --

--- public/js/crop.js
+++ public/js/crop.js
@@ -38,7 +38,7 @@ jQuery(function($){
    // });
 	// Изменение координат
 	function showCoords(c){
-		x1 = c.x+69; $('#x1').val(c.x);		
+		x1 = c.x+49; $('#x1').val(c.x);		
 		y1 = c.y; $('#y1').val(c.y);		
 		x2 = c.x2; $('#x2').val(c.x2);		
 		y2 = c.y2; $('#y2').val(c.y2);
