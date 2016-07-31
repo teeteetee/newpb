7c92f0759976fedbe917c7a441db1f5e496914d7
teeteetee <proto@protos-MacBook-Pro.local>
2016-07-31 04:10:25 -0700

    --

--- app.js
+++ app.js
@@ -85,7 +85,7 @@ app.get('/counter/initdb',function (req,res){
   stats.remove({},function(err,done){
   stats.insert({queryhook:'stats',newmovies:0,totalmovies:0,seenmovies:0});
   stats.find({},function(err,done){
-    res.send(done);
+    res.redirect('/counter');
   });
  });
 });
@@ -105,7 +105,7 @@ app.get('/counter/showstats',function (req,res){
 app.get('/counter/deletemovies',function (req,res){
   movies.remove({},function(err,done){
   stats.update({queryhook:'stats'},{$set:{newmovies:0,totalmovies:0,seenmovies:0}});
-    res.send('done');
+    res.redirect('/counter/initdb');
   });
 });
 
@@ -154,8 +154,7 @@ app.post('/counter/rm_movie/:newmovie/:movie_id',function(req,res){
   var vnewmovie = req.params.newmovie;
   if(vmovie_id) {
     movies.remove({_id:vmovie_id},function(err,done){
-      if(!err) {
-        ms.trouble=0;
+      if(err) {
         res.send(ms);
       }
       else {
