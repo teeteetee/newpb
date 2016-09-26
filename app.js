65f71db977e94c46aa2f9281d79a22cb74e78ec9
teeteetee <proto@protos-MacBook-Pro.local>
2016-09-26 02:51:55 -0700

    --

--- app.js
+++ app.js
@@ -170,7 +170,7 @@ app.post('/counter/getbooks',function (req,res){
   var ms ={};
   ms.trouble=1;
   if(req.session&&req.session._id)
-  {counter_books.find({uid:JSON.stringify(req.session._id)},function(err,doc){
+  {counter_books.findOne({uid:JSON.stringify(req.session._id)},function(err,doc){
       if(err) {
         console.log('ERR WHILE MOVIES QUERY');
         res.send(ms);
