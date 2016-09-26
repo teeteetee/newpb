70ab604aa1be500ca1a27a0cc70b2a458f37b2f9
teeteetee <proto@protos-MacBook-Pro.local>
2016-09-26 01:17:01 -0700

    --

--- app.js
+++ app.js
@@ -152,6 +152,7 @@ app.post('/counter/getmovies',function (req,res){
         res.send(ms);
       }
       else if(doc!=null){
+        console.log(doc);
         ms.doc = doc;
         ms.trouble=0;
         res.send(ms);
