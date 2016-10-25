f0580c3eb0da0ee64585c83cf59df2be5bce8977
teeteetee <proto@protos-MacBook-Pro.local>
2016-10-25 11:40:49 -0700

    --

--- app.js
+++ app.js
@@ -627,12 +627,14 @@ app.post('/counter/removefriend',function (req,res){
       console.log('err removing a friend');
       res.send(ms);}
       else {
+        console.log('step 1');
         if(done.total<0){
           counter_friends.update({uid:JSON.stringify(req.session._id)},{$set:{total:0}},function (err2,done2){
             if(err){
             console.log('err correcting total');
             res.send(ms);}
-            else {
+            else { 
+            console.log('step 2');
              ms.trouble=0;
              res.send(ms);
             }
