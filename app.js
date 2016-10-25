14ee1518917579545863d5886f852858ed5cf14d
teeteetee <proto@protos-MacBook-Pro.local>
2016-10-25 11:32:50 -0700

    --

--- app.js
+++ app.js
@@ -622,7 +622,7 @@ app.post('/counter/removefriend',function (req,res){
  {var ms={};
   ms.trouble=1;
   console.log('removing a friend');
-  counter_friends.update({uid:JSON.stringify(req.session._id)},{$pull:{friendstore:{_id:req.body._id}},$inc:{total:-1}},function (err,done){
+  counter_friends.update({uid:JSON.stringify(req.session._id)},{$pull:{friendstore:{_id:req.body._id.toString()}},$inc:{total:-1}},function (err,done){
     if(err){
       console.log('err removing a friend');
       res.send(ms);}
