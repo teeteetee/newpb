c3629181ded3e7aa35ced3072ec2eaffd313f544
teeteetee <proto@protos-MacBook-Pro.local>
2016-12-13 02:35:48 -0800

    --

--- app.js
+++ app.js
@@ -931,7 +931,7 @@ app.post('/counter/addfriend',function (req,res){
       res.send(ms);}
       else {
         conso.log('adding a follower');
-        counter_friends.update({uid:req.body._id},{$push:{followers:JSON.stringify(req.session._id)},$inc:{total_fd:1}},function (err,done){
+        counter_friends.update({uid:req.body._id},{$push:{followers:JSON.stringify(req.session._id)},$inc:{total_fl:1}},function (err,done){
           if(err){
             console.log('err adding a friend 2');
             res.send(ms);}
@@ -954,7 +954,7 @@ app.post('/counter/removefriend',function (req,res){
  {var ms={};
   ms.trouble=1;
   console.log('removing a friend');
-  counter_friends.update({uid:JSON.stringify(req.session._id)},{$pull:{friendstore:req.body._id.toString()},$inc:{total_fl:-1}},function (err,done){
+  counter_friends.update({uid:JSON.stringify(req.session._id)},{$pull:{friendstore:req.body._id.toString()},$inc:{total_fd:-1}},function (err,done){
     if(err){
       console.log('err removing a friend');
       res.send(ms);}
