bc084694cf5c8f4b7caeeb766293a8a9eeee384e
teeteetee <proto@protos-MacBook-Pro.local>
2017-09-23 02:24:48 -0700

    --

--- app.js
+++ app.js
@@ -610,27 +610,28 @@ app.post('/counter/getteamlists',function (req,res){
     res.send(ms);
   }
   });
-app.get('/counter/checkdb',function (req,res){
-  var ms ={};
-  ms.trouble=1;
-  var fixed_id= new ObjectID('59c56968db564a4e78000001');
-counter_items.findOne({'uid':fixed_id},function(err,doc){
-  if(err) {
-        console.log('ERR WHILE MOVIES QUERY');
-        res.send(ms);
-      }
-      else if(doc!=null){
-        //console.log(doc);
-        ms.doc = doc;
-        ms.trouble=0;
-        res.send(ms);
-      }
-      else {
-        console.log('hey 2: '+doc);
-        res.send(ms);
-      }
-});
-});
+
+//app.get('/counter/checkdb',function (req,res){
+//  var ms ={};
+//  ms.trouble=1;
+//  var fixed_id= new ObjectID('59c56968db564a4e78000001');
+//counter_items.findOne({'uid':fixed_id},function(err,doc){
+//  if(err) {
+//        console.log('ERR WHILE MOVIES QUERY');
+//        res.send(ms);
+//      }
+//      else if(doc!=null){
+//        //console.log(doc);
+//        ms.doc = doc;
+//        ms.trouble=0;
+//        res.send(ms);
+//      }
+//      else {
+//        console.log('hey 2: '+doc);
+//        res.send(ms);
+//      }
+//});
+//});
 
 app.post('/counter/getitems',function (req,res){
   var ms ={};
@@ -638,7 +639,8 @@ app.post('/counter/getitems',function (req,res){
   //var vuid = req.body._id.replace(/"/g,'').trim();
   //console.log('vuid: '+vuid);
   if(req.session&&req.session._id&&req.body._id)
-  {counter_items.findOne({'uid':req.body._id.ObjectID()},function(err,doc){
+  {var temp_id=new ObjectID(req.body._id);
+    counter_items.findOne({'uid':temp_id},function(err,doc){
       if(err) {
         console.log('ERR WHILE MOVIES QUERY');
         res.send(ms);
