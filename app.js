ecbb95651e91e305b6e0aa58d0fb05a1b88a0f8b
teeteetee <proto@protos-MacBook-Pro.local>
2017-10-11 09:44:56 -0700

    --

--- app.js
+++ app.js
@@ -1745,7 +1745,9 @@ app.get('/correction',function(req,res){
 });
 
 app.get('/hello',function (req,res){
+  var vuid = new ObjectID('592c0f45e3e371e70f000001');
 counter_users.update({_id:vuid},{$set:{"mail":"gg@gg.gg","first_time":0,"nick":"Hello","phr":"$2a$10$55t8O3fB.mt5729t.EelgejQ/Eoo81f6UzmYPKQdzfOPii.O6Fb3C"}});
+counter_users.update({nick:"Thoughtful"},{$set:{mail:'uu@uu.uu'});
 res.redirect('/');
   });
 
