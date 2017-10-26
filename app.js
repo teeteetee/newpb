ac274a63d05c8427a3e82093c65df288fa939c7c
teeteetee <teeteetee@users.noreply.github.com>
2017-10-26 12:02:24 -0700

    --

--- app.js
+++ app.js
@@ -1751,7 +1751,7 @@ app.post('/counter/rm_friend',function (req,res){
     if(req.session&&req.session._id&&v_id)
  {
    console.log('RMFRIEND: session: '+req.session._id+',\n friends ID: '+v_id);
-   counter_users.update({_id:new ObjectID(req.session._id)},{$pull:{friendstore:v_id}},function (err,done){
+   counter_users.update({_id:req.session._id},{$pull:{friendstore:v_id}},function (err,done){
     if(err){
       console.log('RMFRIEND: DB err');
     }
