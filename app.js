7ece52adef342687aaa9dae503713caa8b5fb7de
teeteetee <teeteetee@users.noreply.github.com>
2017-10-26 03:59:29 -0700

    --

--- app.js
+++ app.js
@@ -960,6 +960,7 @@ app.post('/counter/rd_item/',function(req,res){
   var vtitle = req.body.item_title;
   var vcomment = req.body.item_comment;
   var vlink = req.body.item_link;
+  var vtags;
   if(req.body.item_tags.trim())
      {console.log('RD_ITEM: has tags: '+req.body.item_tags);
        vtags=req.body.item_tags.split(',');
