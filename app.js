d4a98df4342344302a5efb2cc5c2d2074bd0c976
teeteetee <teeteetee@users.noreply.github.com>
2017-10-26 03:55:24 -0700

    --

--- app.js
+++ app.js
@@ -960,7 +960,13 @@ app.post('/counter/rd_item/',function(req,res){
   var vtitle = req.body.item_title;
   var vcomment = req.body.item_comment;
   var vlink = req.body.item_link;
-  var vtags = req.body.item_tags.trim().split(',');
+  if(req.body.item_tags.trim())
+     {console.log('RD_ITEM: has tags: '+req.body.item_tags);
+       vtags=req.body.item_tags.split(',');
+       vtags.forEach(function (element, index){
+     vtags[index]=trim1(element);
+   });
+     }
   var v_index = req.body.v_index;
   var vtmstmp= parseInt(req.body.item_tmstmp);
   var teamlist = parseInt(req.body.teamlist);
