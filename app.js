d35f873b6f89b81c3de13849e299ab46d0621cea
teeteetee <teeteetee@users.noreply.github.com>
2017-11-29 00:35:20 -0800

    kjbk

--- app.js
+++ app.js
@@ -726,7 +726,7 @@ app.post('/additem',function (req,res){
             if(req.body.item_tags)
               {console.log('ADDITEM: has tags: '+req.body.item_tags);
                console.log('ADDITEM: typeof tags: '+typeof req.body.item_tags);
-                vtags=req.body.item_tags.split(',');
+                //vtags=req.body.item_tags.split(',');
                 console.log('ADDITEM: vtags: '+vtags);
                 vtags.forEach(function (element, index){
               vtags[index]=trim1(element);
